e9563e349c89efb8eedef15d0ae0e6d28f070971
AdaoBJr <adaobjr@gmail.com>
2022-04-03 11:42:41 -0400

    feat: :tada: added inittial files - Button Component

---
+++ components/Button/button.js
@@ -0,0 +1,11 @@
+import './button.css';
+
+function Button(props) {
+  return (
+    <div className="root">
+      <button className="button">{props.children}</button>;
+    </div>
+  );
+}
+
+export default Button;
