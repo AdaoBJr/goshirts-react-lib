b70c2c0060406f53ac990163e38e4f02f6d175be
AdaoBJr <adaobjr@gmail.com>
2022-04-05 15:05:41 -0400

    feat: :hammer: updated button component

--- components/Button/button.js
+++ components/Button/button.js
@@ -1,9 +1,14 @@
-import './button.css';
+import defaultClasses from './button.modules.css';
+
+import { mergeClasses } from '../../talons';
+
+const { useStyles } = mergeClasses();
 
 function Button(props) {
+  const classes = useStyles(defaultClasses, props.classes);
   return (
-    <div className="root">
-      <button className="button">{props.children}</button>
+    <div className={classes.root}>
+      <button className={classes.default}>{props.children}</button>
     </div>
   );
 }
