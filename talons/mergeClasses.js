6f6205fb259578fe5e5d8331d7a1fe2273692c35
AdaoBJr <adaobjr@gmail.com>
2022-04-06 14:01:44 -0400

    feat: :art: updated name func -> useStyles -> useStyle

--- talons/mergeClasses.js
+++ talons/mergeClasses.js
@@ -1,12 +1,12 @@
 import { shallowMerge } from '../utils';
 
 const mergeClasses = () => {
-  const useStyles = (defaultClasses, classes, className) => {
+  const useStyle = (defaultClasses, classes, className) => {
     const hasClassName = className ? { root: className } : null;
     return shallowMerge(defaultClasses, classes, hasClassName);
   };
 
-  return { useStyles };
+  return { useStyle };
 };
 
 export default mergeClasses;
