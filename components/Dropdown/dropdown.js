2aeca984125e0f987ac16bc9ded25f6e258fcdc9
AdaoBJr <adaobjr@gmail.com>
2022-04-08 12:58:31 -0400

    feat: :sparkles: added element openMenu / closeMenu on dropdown

--- components/Dropdown/dropdown.js
+++ components/Dropdown/dropdown.js
@@ -21,21 +21,23 @@ function Dropdown(props) {
         onClick={() => setExpanded((prevState) => !prevState)}
       >
         <span>{activeLabel}</span>
-        <Icon icon={icon || ExpandLess} size={23} />
+        <Icon icon={icon || ExpandLess} active={expanded} size={23} />
       </div>
       <div ref={elementRef} className={expanded ? classes.itemsActive : classes.items}>
-        {items.map(({ key, label, value }) => {
-          return (
-            <div
-              aria-hidden
-              key={key}
-              className={activeLabel === label ? classes.itemActive : classes.item}
-              onClick={() => handleClick(value)}
-            >
-              <span>{label}</span>
-            </div>
-          );
-        })}
+        <div className={expanded ? classes.openMenu : classes.closeMenu}>
+          {items.map(({ key, label, value }) => {
+            return (
+              <div
+                aria-hidden
+                key={key}
+                className={activeLabel === label ? classes.itemActive : classes.item}
+                onClick={() => handleClick(value)}
+              >
+                <span>{label}</span>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
