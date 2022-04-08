c01492d19ec6ab10051ce98554dd244df5c5d9c4
AdaoBJr <adaobjr@gmail.com>
2022-04-08 09:02:03 -0400

    feat: :art: updated Icon Component

--- components/Icon/icon.js
+++ components/Icon/icon.js
@@ -1,18 +1,21 @@
-import { useStyle, useWindowSize } from '../../services/hooks';
+import { useStyle } from '../../services/hooks';
 import defaultClasses from './icon.module.css';
 
 function Icon(props) {
-  const { src, size, width } = props;
+  const { icon: IconRender, size, width, color, stroke, fill } = props;
   const classes = useStyle(defaultClasses, props.classes);
-
-  const windowSize = useWindowSize();
-  console.log(windowSize);
 
   return (
     <span className={classes.root}>
-      <img src={src} width={size || width} className={classes.defaultStyles}>
-        {props.children}
-      </img>
+      <IconRender
+        style={{
+          fontSize: size || width,
+          stroke,
+          color,
+          fill,
+        }}
+        className={classes.icon}
+      />
     </span>
   );
 }
