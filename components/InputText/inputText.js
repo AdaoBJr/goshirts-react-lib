7ccdc7d430a8545df6f4b3e2c19cdd16e6d20a68
AdaoBJr <adaobjr@gmail.com>
2022-04-08 13:42:54 -0400

    feat: :sparkles: added label with id on inputText

--- components/InputText/inputText.js
+++ components/InputText/inputText.js
@@ -2,21 +2,22 @@ import { useStyle } from '../../services/hooks';
 import defaultClasses from './inputText.module.css';
 
 function InputText(props) {
-  const { name, value, placeholder, label, text, children } = props;
+  const { id, name, value, placeholder, label, text, children } = props;
   const classes = useStyle(defaultClasses, props.classes);
 
   return (
     <div className={classes.root}>
-      <label className={classes.label}>
+      <label className={classes.label} htmlFor={id}>
         {label}
-        <input
-          type="text"
-          name={name}
-          value={value}
-          placeholder={placeholder}
-          className={classes.input}
-        />
       </label>
+      <input
+        id={id || name || label}
+        type="text"
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        className={classes.input}
+      />
       <span className={classes.text}>{text || children}</span>
     </div>
   );
