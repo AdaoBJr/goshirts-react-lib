8f196260f72f0e025e8986da9584d82d41f2d9ee
AdaoBJr <adaobjr@gmail.com>
2022-04-10 17:07:13 -0400

    feat: :zap: added props id / onClick on Icon Component

--- components/Icon/icon.js
+++ components/Icon/icon.js
@@ -2,13 +2,15 @@ import { useStyle } from '../../services/hooks';
 import defaultClasses from './icon.module.css';
 
 function Icon(props) {
-  const { icon: IconRender, active, size, width, ...restProps } = props;
+  const { id, icon: IconRender, active, size, width, onClick, ...restProps } = props;
   const classes = useStyle(defaultClasses, props.classes);
 
   return (
     <span className={classes.root}>
       {IconRender && (
         <IconRender
+          id={id}
+          onClick={onClick}
           style={{ fontSize: size || width, ...restProps }}
           className={active ? classes.iconActive : classes.icon}
         />
