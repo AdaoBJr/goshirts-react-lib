0dd608aa1d77e4357576fc773afe85b081fdf5ba
AdaoBJr <adaobjr@gmail.com>
2022-04-09 23:55:32 -0400

    refactor: :zap: refacted props on Icon

--- components/Icon/icon.js
+++ components/Icon/icon.js
@@ -2,28 +2,13 @@ import { useStyle } from '../../services/hooks';
 import defaultClasses from './icon.module.css';
 
 function Icon(props) {
-  const {
-    icon: IconRender,
-    active,
-    size,
-    width,
-    color,
-    stroke,
-    fill,
-    ...restProps
-  } = props;
+  const { icon: IconRender, active, size, width, ...restProps } = props;
   const classes = useStyle(defaultClasses, props.classes);
 
   return (
     <span className={classes.root}>
       <IconRender
-        style={{
-          fontSize: size || width,
-          stroke,
-          color,
-          fill,
-          ...restProps,
-        }}
+        style={{ fontSize: size || width, ...restProps }}
         className={active ? classes.iconActive : classes.icon}
       />
     </span>
