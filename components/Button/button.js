9bc1f49c2a881443f55572652233e417e32fd715
AdaoBJr <adaobjr@gmail.com>
2022-04-10 00:41:08 -0400

    style: :lipstick: updated component Button

--- components/Button/button.js
+++ components/Button/button.js
@@ -2,11 +2,17 @@ import { useStyle } from '../../services/hooks';
 import defaultClasses from './button.module.css';
 
 function Button(props) {
+  const { id, type, label, children, ...restProps } = props;
   const classes = useStyle(defaultClasses, props.classes);
 
   return (
     <div className={classes.root}>
-      <button className={classes.defaultStyles}>{props.children}</button>
+      <label className={classes.label} htmlFor={id || label}>
+        {label}
+      </label>
+      <button id={id || label} type={type} className={classes.button} {...restProps}>
+        {children}
+      </button>
     </div>
   );
 }
